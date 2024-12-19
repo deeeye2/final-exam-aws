1be23ed8aadde770b56ae4d9f54d55486ed776cb
Mohamed Sesay <mohamed.sesay@outlook.com.au>
2024-12-19 18:59:52 +0100

    changies to files

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,10 @@ app.get('/api', (req, res) => {
   res.json({ message: 'Hello, World! Welcome to our website backend.' });
 });
 
-// Serve Static Frontend Files
-app.use(express.static('../frontend'));
+// Serve Static Frontend Files (optional if running in the same container)
+// app.use(express.static('../frontend'));
 
 app.listen(port, () => {
   console.log(`Backend server running at http://localhost:${port}`);
 });
+
